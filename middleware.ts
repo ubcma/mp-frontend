7452acd3efc88b74b17f7065fd03308e38631191
ETHAN <ethan.t.hansen@gmail.com>
2025-04-30 01:50:39 -0700

    feat: block access to app in prod

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,24 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+
   const token = request.cookies.get('membership-portal.session_token')?.value;
   const { pathname } = request.nextUrl;
+
+  if (process.env.NEXT_PUBLIC_NODE_ENV === 'production') {
+    const allowlist = [
+      '/maintenance',
+      '/favicon.ico',
+      '/robots.txt',
+      '/_next', // static files
+      '/api',
+    ];
+    const isAllowed = allowlist.some(path => pathname.startsWith(path));
+    if (!isAllowed) {
+      return NextResponse.redirect(new URL('/maintenance', request.url));
+    }
+    return NextResponse.next();
+  }
 
   if (pathname === '/sign-in' || pathname === '/sign-up' || pathname === '/') {
     if (token) {
@@ -25,6 +41,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    '/:path',
     '/',
     '/home/:path*', 
     '/events/:path*',  
