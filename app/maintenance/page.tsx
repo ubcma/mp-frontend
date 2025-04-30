21f680088d122f1a5d0b77d6fcdab59eb8de8f3f
ETHAN <ethan.t.hansen@gmail.com>
2025-04-30 01:50:39 -0700

    feat: block access to app in prod

---
+++ app/maintenance/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-2">
+      <img src='/logos/logo_red.svg' width={164} height={164}/>
+      <h1 className="text-xl font-semibold">UBC Marketing Association</h1>
+      <p className="text-sm text-muted-foreground"> Our portal is currently under development. We'll see you in September!</p>
+    </div>
+  );
+
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <h1 className="text-2xl font-bold">Home Page</h1>
-    </div>
-  );
-
-}
