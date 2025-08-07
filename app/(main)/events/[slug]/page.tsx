23bd708cac6bfdf6bf9b4576e8758595bf442905
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2025-08-07 00:31:19 -0700

    updated gitignore + page to include loading page

--- app/(main)/events/[slug]/page.tsx
+++ app/(main)/events/[slug]/page.tsx
@@ -3,13 +3,21 @@
 import { useGetEventQuery } from '@/lib/queries/event';
 import { useParams } from 'next/navigation';
 import RenderEventDetails from '@/components/EventDetails';
+import { Skeleton } from '@/components/ui/skeleton';
+
 
 export default function EventPage() {
   const { slug } = useParams<{ slug: string }>();
   const { data, isLoading, isError } = useGetEventQuery({ eventSlug: slug });
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {[...Array(3)].map((_, i) => (
+          <Skeleton key={i} className="h-[400px] w-full rounded-xl" />
+        ))}
+      </div>
+    );
   }
 
   if (isError || !data) {
