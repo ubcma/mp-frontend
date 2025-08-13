d6b8da5bc86596605c777d0225474f12eddfc4e1
Ethan Hansen <ethan.t.hansen@gmail.com>
2025-08-13 16:25:50 -0700

    fix: mobile layout, checkoutform redirect for apple pay

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,18 +1,18 @@
-import { AuthPageGradient } from '@/components/BackgroundGradients';
+import { AuthPageGradient } from "@/components/BackgroundGradients";
 
 export default function Layout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <div
       className="
-    flex justify-center items-center
-    absolute top-0 left-0 w-full
-    h-screen md:relative md:h-screen
-    overflow-hidden overscroll-none
-  "
+        flex justify-center items-center
+        fixed top-0 left-0 w-full
+        h-[100dvh] md:relative md:h-screen
+        overflow-hidden overscroll-none
+      "
     >
       {/* Background Gradient */}
       <AuthPageGradient />
@@ -20,16 +20,15 @@ export default function Layout({
       {/* Auth Content */}
       <div
         className="
-      w-full md:w-fit place-self-center
-      flex flex-col justify-center items-center
-      h-full fixed md:relative md:h-fit
-      md:px-12 px-8 py-12
-      bg-background/60 bg-blend-hard-light backdrop-blur-xl
-      md:border border-background/80 md:m-4 md:rounded-md
-      shadow-[inset_0px_0px_80px_20px_rgba(255,255,255,0.1)]
-      dark:shadow-[inset_0px_0px_80px_20px_rgba(0,0,0,0.1)]
-      md:py-12
-    "
+          w-full md:w-fit
+          flex flex-col justify-center items-center
+          h-full md:h-fit
+          md:px-12 px-8 py-12
+          bg-background/60 bg-blend-hard-light backdrop-blur-xl
+          md:border border-background/80 md:m-4 md:rounded-md
+          shadow-[inset_0px_0px_80px_20px_rgba(255,255,255,0.1)]
+          dark:shadow-[inset_0px_0px_80px_20px_rgba(0,0,0,0.1)]
+        "
       >
         {children}
       </div>
