f440f6a3eb4fc8925750aff0ed1d6d4a8965ae61
Ethan Hansen <ethan.t.hansen@gmail.com>
2025-10-15 16:35:20 -0700

    hotfix: block job board access for non-members

--- app/(members)/job-board/page.tsx
+++ app/(members)/job-board/page.tsx
@@ -1,13 +1,31 @@
-'use client'
+'use client';
 
-import { JobCard } from "@/components/JobCard";
-import { useGetJobsQuery } from "@/lib/queries/jobs";
+import { JobCard } from '@/components/JobCard';
+import { useGetJobsQuery } from '@/lib/queries/jobs';
+import { useUserQuery } from '@/lib/queries/user';
+import { use } from 'react';
 
 export default function JobBoard() {
-  const {data, isLoading, isError} = useGetJobsQuery();
+  const { data, isLoading, isError } = useGetJobsQuery();
+
+  const { data: user } = useUserQuery();
 
   // Filter for only active jobs
-  const activeJobs = data?.filter(job => job.isActive) || [];
+  const activeJobs = data?.filter((job) => job.isActive) || [];
+
+  if (user?.role === 'Basic') {
+    return (
+      <div className="w-full h-full flex flex-col gap-4">
+        <h1 className="font-bold text-2xl text-ma-red">
+          This feature is exclusive for members.
+        </h1>
+        <p className="text-muted-foreground">
+          Please upgrade your account
+          to access job listings and other exclusive perks.
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div className="w-full h-full flex flex-col gap-4">
@@ -16,10 +34,12 @@ export default function JobBoard() {
       {isLoading ? (
         <p>Loading jobs...</p>
       ) : activeJobs.length > 0 ? (
-        activeJobs.map((job, idx) => <JobCard job={job} key={idx}/>)
+        activeJobs.map((job, idx) => <JobCard job={job} key={idx} />)
       ) : (
-        <p className="text-muted-foreground">No active jobs available at the moment.</p>
+        <p className="text-muted-foreground">
+          No active jobs available at the moment.
+        </p>
       )}
     </div>
-  )
+  );
 }
