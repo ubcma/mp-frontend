d87f6d25051cbdc09fd20a23c97b8ec44e43a511
janayee-c <janaye.c13@gmail.com>
2025-10-22 19:17:25 -0700

    attendee cap - frontend stripe

--- lib/queries/users.ts
+++ lib/queries/users.ts
@@ -1,10 +1,31 @@
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchFromAPI } from '../httpHandlers';
 import { UserProfileData } from '../types';
+import { toast } from 'sonner';
+import { handleClientError } from '../error/handleClient';
 
-export function useGetAllUsersQuery() {
-  return useQuery({
-    queryKey: ['users'],
+export type PaginatedResponse<U> = {
+  data: U[];
+  meta: {
+    page: number;
+    pageSize: number;
+    totalCount: number;
+    totalPages: number;
+    filters?: {
+      role?: string | null;
+      search?: string | null;
+    };
+  };
+};
+
+export function useGetAllUsersQuery(
+  page: number,
+  pageSize: number,
+  role?: string,
+  search?: string
+) {
+  return useQuery<PaginatedResponse<UserProfileData>>({
+    queryKey: ['users', page, pageSize, role, search],
     queryFn: async () => {
       const res = await fetchFromAPI('/api/users', {
         method: 'GET',
@@ -20,5 +41,38 @@ export function useGetAllUsersQuery() {
     },
     retry: 1,
     staleTime: 5 * 60 * 1000,
+  });
+}
+
+
+export function useUpdateUserMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({
+      userId,
+      newRole,
+    }: {
+      userId: string;
+      newRole: string;
+    }) => {
+      const res = await fetchFromAPI(`/api/users/${userId}/role`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        body: { newRole },
+      });
+
+      if (!res.ok) {
+        throw new Error('Failed to update user role');
+      }
+
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] });
+      toast.success('User role updated!');
+    },
+    onError: (err: unknown) => handleClientError('Error updating user role', err),
   });
 }
