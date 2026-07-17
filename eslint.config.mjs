f3daf041e01b8e954820c523407480e3a5418053
Kevin Gu <kevinxygu@gmail.com>
2026-07-17 14:13:24 -0700

    Updated stale ESLINT

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import next from "@next/eslint-plugin-next";
 
 export default defineConfig([
   
